67d6ca7d310cc277171585466fb396734f23ca2f
support@seolib.ru <support@seolib.ru>
2015-03-02 12:56:41 +0200

    HW2

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest, random
+from classes.group import Group
 
 
 def is_alert_present(wd):
@@ -19,29 +20,29 @@ class test_add_group(unittest.TestCase):
     def open_main_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login_as(self, wd):
+    def login_as(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
-    def add_new_group(self, wd):
+    def add_new_group(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("Friends")
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("My friends")
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("for cyrrent time")
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
 
     def return_to_group_page(self, wd):
@@ -51,15 +52,15 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_link_text("Logout").click()
 
     def test_test_add_group(self):
-        success = True
         wd = self.wd
         self.open_main_page(wd)
-        self.login_as(wd)
+        self.login_as(wd, "admin", "secret")
         self.open_groups_page(wd)
-        self.add_new_group(wd)
+        test_group = Group("Friends" + str(random.randint(0,1000000)), "My friends", "for current time")
+        self.add_new_group(wd, test_group)
         self.return_to_group_page(wd)
         self.logout(wd)
-        self.assertTrue(success)
+
 
     def tearDown(self):
         self.wd.quit()
