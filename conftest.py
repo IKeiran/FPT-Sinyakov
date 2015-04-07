f1f712096aaa10aa85704c94d4d7e8e777f255f4
support@seolib.ru <support@seolib.ru>
2015-04-07 16:13:52 +0300

    Добавлен конфигурационный файл

--- conftest.py
+++ conftest.py
@@ -1,19 +1,22 @@
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
+target = None
 
 @pytest.fixture
 def app(request):
-    global fixture
-    if fixture is None:
-        browser = request.config.getoption('--browser')
-        baseUrl = request.config.getoption('--baseUrl')
-        fixture = Application(browser, baseUrl)
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", userpassword="secret")
+    global fixture, target
+    browser = request.config.getoption('--browser')
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--target'))
+        with open(config_file) as f:
+            target = json.load(f)
+    if (fixture is None) or (not fixture.is_valid()):
+        fixture = Application(browser, target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], userpassword=target["password"])
     return fixture
 
 
@@ -27,5 +30,5 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='firefox')
-    parser.addoption('--baseUrl', action='store', default='http://localhost/addressbook/')
+    parser.addoption('--target', action='store', default='target.json')
 
