6b0cf25c72f93b371977418aa3dc4cc001f1ebe5
ikeiran <mic.sinyakov@gmail.com>
2015-04-12 21:25:33 +0300

    добавлен тест на сравнение групп через пользовательский интерфейс и БД

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from fixture.application import Application
+from fixture.db import DBFixture
 import json
 import os.path
 import importlib
@@ -10,24 +11,33 @@ target = None
 
 @pytest.fixture
 def app(request):
-    global fixture, target
+    global fixture
     browser = request.config.getoption('--browser')
-    if target is None:
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--target'))
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption('--target'))['web']
     if (fixture is None) or (not fixture.is_valid()):
-        fixture = Application(browser, target["baseUrl"])
-    fixture.session.ensure_login(username=target["username"], userpassword=target["password"])
+        fixture = Application(browser, web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], userpassword=web_config["password"])
     return fixture
 
 
-@pytest.fixture(scope='session', autouse=True)
-def stop(request):
-    def fin():
-        fixture.session.logout()
-        fixture.destroy()
-    request.addfinalizer(fin)
+@pytest.fixture(scope='session')
+def db(request):
+    db_config = load_config(request.config.getoption('--target'))['db']
+    db_fixture = DBFixture(host=db_config["host"], name=db_config["name"],
+                           user=db_config["user"], password=db_config["password"])
+    def stop():
+        db_fixture.destroy()
+    request.addfinalizer(stop)
+    return db_fixture
+
+
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
 
 
 def pytest_addoption(parser):
@@ -44,8 +54,10 @@ def pytest_generate_tests(metafunc):
             testdata = load_from_json(fixture[5:])
             metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
 
+
 def load_from_module(module):
     return importlib.import_module("data.%s" % module).testdata
+
 
 def load_from_json(file):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as data_file:
---
+++ fixture/db.py
@@ -0,0 +1,27 @@
+__author__ = 'Keiran'
+import mysql.connector
+from model.group import Group
+
+class DBFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        cursor = self.connection.cursor()
+        group_list = []
+        try:
+            cursor.execute('select group_id, group_name, group_header, group_footer from group_list')
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return group_list
+
+    def destroy(self):
+        self.connection.close()
