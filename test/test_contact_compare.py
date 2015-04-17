5cb39cc9538c57d57301e1569bfb6688e77f9168
support@seolib.ru <support@seolib.ru>
2015-04-17 10:26:29 +0300

    Контакты на главной странице сверяются с информацией, полученной из БД

--- test/test_contact_compare.py
+++ test/test_contact_compare.py
@@ -1,11 +1,8 @@
 __author__ = 'Keiran'
-from random import randrange
+from model.contact import Contact
 
 
-def test_contact_compare(app):
-    index = randrange(app.contact.count())
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page == contact_from_edit_page
-    assert contact_from_home_page.all_mails == contact_from_edit_page.join_mails()
-    assert contact_from_home_page.all_phones == contact_from_edit_page.join_phones()
+def test_contact_compare(app, db):
+    contacts_from_db = db.get_contact_list()
+    contacts_from_home_page = app.contact.get_contact_list()
+    assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
