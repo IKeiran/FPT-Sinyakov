79311481c4d68213a6e097362c1a920127eb896b
support@seolib.ru <support@seolib.ru>
2015-04-17 17:52:33 +0300

    Подправлено сравнение контактов. Нужно вывести на сравнение списков

--- test/test_contact_compare.py
+++ test/test_contact_compare.py
@@ -6,3 +6,6 @@ def test_contact_compare(app, db):
     contacts_from_db = db.get_contact_list()
     contacts_from_home_page = app.contact.get_contact_list()
     assert sorted(contacts_from_home_page, key=Contact.id_or_max) == sorted(contacts_from_db, key=Contact.id_or_max)
+
+    assert contacts_from_home_page.all_mails == contacts_from_db.join_mails()
+    assert contacts_from_home_page.all_phones == contacts_from_db.join_phones()
