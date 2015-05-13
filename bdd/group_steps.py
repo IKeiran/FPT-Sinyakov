e7257197eefe689450cfc13b231581dec5fbba86
support@seolib.ru <support@seolib.ru>
2015-05-13 12:38:39 +0300

    add allure steps for bdd tests

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,11 +1,14 @@
 from pytest_bdd import given, when, then
 from model.group import Group
 import random
+import pytest
 
+@pytest.allure.step("Given a group list")
 @given("a group list")
 def group_list(db):
     return db.get_group_list()
 
+@pytest.allure.step("Given group with name={name}, header = {header} and footer={footer}")
 @given("a group with <name>,<header> and <footer>")
 def new_group(name, header, footer):
     return Group(name,header, footer)
@@ -13,35 +16,42 @@ def new_group(name, header, footer):
 
 @when("I add the group to the list")
 def add_new_group(app, new_group):
-    app.group.create(new_group)
+    with pytest.allure.step("When I add the group to the list"):
+        app.group.create(new_group)
 
 @then("the new group list is equal to the old list with the added group")
 def verify_group_added(db, group_list, new_group):
-    old_groups = group_list
-    old_groups.append(new_group)
-    new_groups = db.get_group_list()
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step("When the new group list is equal to the old list with the added group"):
+        old_groups = group_list
+        old_groups.append(new_group)
+        new_groups = db.get_group_list()
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
+@pytest.allure.step("Given a non-empty group list")
 @given("a non-empty group list")
 def non_empty_group_list(app, db):
-    if len(db.get_group_list()) ==0:
-        app.group.create(Group(name = 'Some_name'))
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name='Some_name'))
     return db.get_group_list()
 
+@pytest.allure.step("Given a random group from the list")
 @given("a random group from the list")
 def random_group(non_empty_group_list):
     return random.choice(non_empty_group_list)
 
 @when("I delete the group from the list")
 def delete_group_from_list(app, random_group):
-    app.group.delete_by_id(random_group.id)
+    with pytest.allure.step("When I delete the group %s from the list:" % random_group):
+       app.group.delete_by_id(random_group.id)
 
 @then("the new group list is equal to the list without deleted group")
 def verify_group_deleted(non_empty_group_list, db, random_group, app, check_ui):
-    old_groups = non_empty_group_list
-    new_groups = db.get_group_list()
-    assert len(old_groups) - 1 == app.group.count()
-    old_groups.remove(random_group)
-    assert old_groups == new_groups
+    with pytest.allure.step("Then the new group list is equal to the list without deleted group"):
+        old_groups = non_empty_group_list
+        new_groups = db.get_group_list()
+        assert len(old_groups) - 1 == app.group.count()
+        old_groups.remove(random_group)
+        assert old_groups == new_groups
     if check_ui:
-        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+        with pytest.allure.step("Also check UI:"):
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
